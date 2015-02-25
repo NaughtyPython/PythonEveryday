f377f08f32af4ea75762eaf984361a9336d44335
huangaq <huangaq@garena.com>
2015-02-25 15:22:58 +0800

    add 0010 generate random letter checking image

---
+++ src/generateRandomLetter.py
@@ -0,0 +1,25 @@
+import string
+import random
+from path import *
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+
+def generate_random_letter():
+    word = []
+    for _i in range(4):
+        word.append(random.choice(string.letters))
+    return ''.join(word)
+
+
+def generate_letter_image(word):
+    font = ImageFont.truetype(os.path.join(font_path, 'GoodDog.otf'), 120)
+    img = Image.open(os.path.join(assets_path, 'number_1.png'))
+    draw = ImageDraw.Draw(img)
+    print img.size
+#     for each in word:
+    draw.text((300,300), word, (0,0,0), font=font)
+    img.show()
+
+if __name__=='__main__':
+    generate_letter_image(generate_random_letter())
--- src/path.py
+++ src/path.py
@@ -1,3 +1,5 @@
 import os
 base_path = os.path.split(os.path.dirname(__file__))[0]
 assets_path = os.path.abspath(os.path.join(base_path, 'assets'))
+font_path = os.path.abspath(os.path.join(assets_path, 'fonts'))
+image_path = os.path.abspath(os.path.join(assets_path, 'Images'))
